e36a728fe1cc6557a628a343c1cc2c3b325bc65a
Ayoub <boueuayoub@gmail.com>
2026-08-01 02:53:24 +0100

    Add dashboard support center

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "fbb19bc9eaa74ae5"; }
+/******/ 		__webpack_require__.h = function() { return "fa0f7daa58679afa"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
